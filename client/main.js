1ab53cca3c67dac8e31981a493271e796b89eb22
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-17 18:08:28 -0800

    Add db, but not displaying tasks

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- 
+
 import App from '../imports/ui/App.js';
+import '../imports/api/tasks.js';
  
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
